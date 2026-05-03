0c68ea5ef3b7e61529549ba8f153fa376ca4203e
FinalDegree <59392139+FinalDegree@users.noreply.github.com>
2026-05-04 00:43:12 +0300

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -2,13 +2,17 @@ public class SelectionSort implements SortingStrategy {
     @Override
     public void sort(Car[] source) {
         for(int i = 0; i < source.length; i++){
+            int pos = i;
+            var min = source[i];
             for(int j = i + 1; j < source.length; j++){
-                if(source[i].compareTo(source[j]) > 0){
-                    var temp = source[i];
-                    source[i] = source[j];
-                    source[j] = temp;
+                if(min.compareTo(source[j]) > 0){
+                    System.out.println(source[i].compareTo(source[j]));
+                    pos = j;
+                    min = source[j];
                 }
             }
+            source[pos] = source[i];
+            source[i] = min;
         }
     }
 }
