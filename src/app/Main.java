99a0a3a17fd238d424448ca29be4dde3dcbb647f
LeksaaQwerty <164042702+LeksaaQwerty@users.noreply.github.com>
2026-05-07 23:03:28 +0300

    Feature/sort (#9)
    * add sort
    
    * last fix

---
+++ src/app/Main.java
@@ -0,0 +1,35 @@
+package app;
+
+import java.util.Comparator;
+import model.Car;
+import sort.ArraySorted;
+import sort.CarComparator;
+import sort.SelectionSort;
+import utils.CustomArrayList;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        
+
+        CustomArrayList<Car> carList = new CustomArrayList<>();
+        ArraySorted sorter = new ArraySorted(new SelectionSort());
+
+        carList.add(new Car.Builder("BMW M5 Competition").yearOfProduction(2020).hp(600).build());
+        carList.add(new Car.Builder("BMW M5 Com").yearOfProduction(2020).hp(625).build());
+        carList.add(new Car.Builder("Audi RS6").yearOfProduction(2022).hp(600).build());
+        carList.add(new Car.Builder("Mersedes-AMG C63 S").yearOfProduction(2014).hp(510).build());
+        carList.add(new Car.Builder("Tesla Model S").yearOfProduction(2023).hp(1020).build());
+
+        System.out.println(carList);
+
+            Comparator<Car> multiComparator = CarComparator.byModel()
+                    .thenComparing(CarComparator.byYear())
+                    .thenComparing(CarComparator.byHp());
+            
+            sorter.sort(carList, multiComparator);
+        
+        System.out.println(carList);
+        }
+    }
---
+++ src/sort/SortStrategy.java
@@ -0,0 +1,10 @@
+package sort;
+
+import java.util.Comparator;
+import model.Car;
+import utils.CustomArrayList;
+
+public interface SortStrategy {
+
+    void sort(CustomArrayList<Car> car, Comparator<Car> comparator);
+}
