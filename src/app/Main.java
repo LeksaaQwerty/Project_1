56e88c5ab2c91f4a4e91a9fc6ce6b47ef61657bf
Alexey Sheleg <alex138728@gmail.com>
2026-05-07 14:41:01 +0300

    last fix

--- src/app/Main.java
+++ src/app/Main.java
@@ -17,12 +17,13 @@ public class Main {
         ArraySorted sorter = new ArraySorted(new SelectionSort());
 
         carList.add(new Car.Builder("BMW M5 Competition").yearOfProduction(2020).hp(600).build());
-        carList.add(new Car.Builder("BMW M5 Com").yearOfProduction(2020).hp(625).build()); // Та же модель и год, мощнее
+        carList.add(new Car.Builder("BMW M5 Com").yearOfProduction(2020).hp(625).build());
         carList.add(new Car.Builder("Audi RS6").yearOfProduction(2022).hp(600).build());
-        carList.add(new Car.Builder("Mersedes-AMG C63 S").yearOfProduction(2014).hp(510).build()); // Та же модель, год старше
+        carList.add(new Car.Builder("Mersedes-AMG C63 S").yearOfProduction(2014).hp(510).build());
         carList.add(new Car.Builder("Tesla Model S").yearOfProduction(2023).hp(1020).build());
 
-        System.out.println("Сортировка по трем полям...");
+        System.out.println(carList);
+
             Comparator<Car> multiComparator = CarComparator.byModel()
                     .thenComparing(CarComparator.byYear())
                     .thenComparing(CarComparator.byHp());
